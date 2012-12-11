68faceefe7e75829d06b72ca2934b0aed32037d1
N3MIS15 <N3MIS15@gmail.com>
2012-12-11 12:07:20 +1100

    fix library export

--- modules/controls.py
+++ modules/controls.py
@@ -86,6 +86,7 @@ def xhr_play_media(file_type, media_type, media_id):
 
     return jsonify({ 'success': True })
 
+
 @app.route('/xhr/enqueue/<file_type>/<media_type>/<int:media_id>')
 @requires_auth
 def xhr_enqueue_media(file_type, media_type, media_id):
@@ -139,6 +140,7 @@ def xhr_enqueue_media(file_type, media_type, media_id):
 
     return jsonify({ 'success': True })
 
+
 @app.route('/xhr/resume/video/<video_type>/<int:video_id>')
 @requires_auth
 def xhr_resume_video(video_type, video_id):
@@ -187,6 +189,7 @@ def xhr_resume_video(video_type, video_id):
 
     return jsonify({ 'success': True })
 
+
 @app.route('/xhr/play/trailer/<int:movieid>')
 @app.route('/xhr/play/trailer/url/<path:trailer>')
 @requires_auth
@@ -220,6 +223,7 @@ def xhr_play_trailer(movieid=None, trailer=None):
         return jsonify({ 'failed': True })
 
     return jsonify({ 'success': True })
+
 
 @app.route('/xhr/play_file/<file_type>/', methods=['POST'])
 @requires_auth
@@ -262,6 +266,7 @@ def xhr_play_file(file_type):
 
     return jsonify({ 'success': True })
 
+
 @app.route('/xhr/enqueue_file/<file_type>/', methods=['POST'])
 @requires_auth
 def xhr_enqueue_file(file_type):
@@ -285,6 +290,7 @@ def xhr_enqueue_file(file_type):
 
     return jsonify({ 'success': True })
 
+
 @app.route('/xhr/playlist/<int:playerid>/play/<int:position>')
 @requires_auth
 def xhr_playlist_play(playerid, position):
@@ -299,6 +305,7 @@ def xhr_playlist_play(playerid, position):
         logger.log('CONTROLS :: %s' % xbmc_error, 'ERROR')
         return jsonify({'failed': True})
 
+
 @app.route('/xhr/playlist/<int:playlistid>/clear')
 @requires_auth
 def xhr_clear_playlist(playlistid):
@@ -312,6 +319,7 @@ def xhr_clear_playlist(playlistid):
     except:
         logger.log('CONTROLS :: %s' % xbmc_error, 'ERROR')
         return jsonify({'failed': True})
+
 
 @app.route('/xhr/playlist/<int:playlistid>/move_item/<int:position1>/<direction>')
 @requires_auth
@@ -336,6 +344,7 @@ def xhr_move_playlist_item(playlistid, position1, direction):
         logger.log('CONTROLS :: %s' % xbmc_error, 'ERROR')
         return jsonify({'failed': True})
 
+
 @app.route('/xhr/playlist/<int:playlistid>/remove_item/<int:position>')
 @requires_auth
 def xhr_remove_playlist_item(playlistid, position):
@@ -349,6 +358,34 @@ def xhr_remove_playlist_item(playlistid, position):
     except:
         logger.log('CONTROLS :: %s' % xbmc_error, 'ERROR')
         return jsonify({'failed': True})
+
+
+@app.route('/xhr/export/<library>/<method>/', methods=['POST'])
+@requires_auth
+def xhr_export_library(library, method):
+    logger.log('CONTROLS :: Exporting %s library' % library, 'INFO')
+    xbmc = jsonrpclib.Server(server_api_address())
+    params = {'options':{}}
+
+    if method == 'separate':
+        for item in request.form:
+            params['options'][item] = True
+    else:
+        path = request.form['path']
+        path = urllib.unquote(path.encode('ascii')).decode('utf-8')
+        params['options']['path'] = path
+
+    try:
+        if library == 'video':
+            xbmc.VideoLibrary.Export(**params)
+        else:
+            xbmc.AudioLibrary.Export(**params)
+        return jsonify({'success': True})
+
+    except:
+        logger.log('CONTROLS :: %s' % xbmc_error, 'ERROR')
+        return jsonify({'failed': True})
+
 
 @app.route('/xhr/controls/<command>')
 @requires_auth
